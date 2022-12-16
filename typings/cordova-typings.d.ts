c6520b4cc04ba6723a73b55a1f947080e18b3f83
Acosta Simón <acosimonm@gmail.com>
2022-12-16 15:41:11 -0300

    BlankCordova

---
+++ typings/cordova-typings.d.ts
@@ -0,0 +1,8 @@
+
+/// <reference path="..\.vscode\typings\cordova\cordova.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Camera.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\FileTransfer.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Dialogs.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Vibration.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\DeviceOrientation.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\FileSystem.d.ts"/>
